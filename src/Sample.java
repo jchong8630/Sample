d03258c5a0addd0bf409809e30df86160cedc5da
Justin Chong <jchong8630@bths.edu>
2022-09-20 19:58:35 -0400

    Added new print statement

--- src/Sample.java
+++ src/Sample.java
@@ -2,5 +2,6 @@ public class Sample {
     public static void main(String[] args) {
         System.out.println("Justin Chong");
         System.out.println("Avengers");
+        System.out.println("Sushi");
     }
 }
